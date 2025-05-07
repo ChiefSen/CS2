191d90f12274ffda999a540ef7b19f13fc3bb8b8
ChiefSen <sgreenwood26@gcds.net>
2025-05-07 13:59:56 -0400

    Add files via upload

---
+++ vim_temp_converter.py
@@ -0,0 +1,6 @@
+cel_temp = input("Hello! What is the temperature in celsius: ")
+cel_temp_num = int(cel_temp)
+farhen_temp = (cel_temp_num * 1.8) + 32
+print("The temperature in Farhenhiet is:")
+print(farhen_temp)
+
